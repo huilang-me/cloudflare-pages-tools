37e86b725c2cd6b51e918b3e39512d1cd57f6bb7
huilang-me <76546468@qq.com>
2025-09-28 16:28:19 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -35,7 +35,17 @@ async function getIPInfo(request) {
     if (match) queryIP = match[1];
   }
 
-  // 3️⃣ 如果没有指定 IP → 使用访问者 IP + CF 信息
+  // 3️⃣ 如果还是没有，并且 host 以 ip. 开头，尝试取路径第一级作为 IP
+  if (!queryIP && url.hostname.startsWith("ip.")) {
+    const pathMatch = url.pathname.match(/^\/([^\/]+)(\/.*)?$/);
+    if (pathMatch) queryIP = pathMatch[1];
+  }
+
+  // 简单 IPv4 和 IPv6 校验正则
+  const ipv4Regex = /^(25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)){3}$/;
+  const ipv6Regex = /^[0-9a-fA-F:]+$/;
+
+  // 4️⃣ 如果没有指定 IP 或者ip格式不对 → 使用访问者 IP + CF 信息
   if (!queryIP) {
     const ip = request.headers.get("cf-connecting-ip");
     const data = {
@@ -49,6 +59,16 @@ async function getIPInfo(request) {
       tz: cf.timezone || null
     };
     return new Response(JSON.stringify(data, null, 2), {
+      headers: {
+        "content-type": "application/json; charset=utf-8",
+        "access-control-allow-origin": "*"
+      }
+    });
+  } else if (!ipv4Regex.test(queryIP) && !ipv6Regex.test(queryIP)) {
+    return new Response(JSON.stringify({
+      error: "无效的 IP 地址",
+      ip: queryIP
+    }, null, 2), {
       headers: {
         "content-type": "application/json; charset=utf-8",
         "access-control-allow-origin": "*"

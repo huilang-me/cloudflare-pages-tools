9a963d489079809598ad987286f7c6f4027d6ede
huilang-me <76546468@qq.com>
2025-09-28 16:32:44 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,12 +3,13 @@ export default {
     const url = new URL(request.url);
     const host = url.hostname;
     const path = url.pathname;
+    const params = url.searchParams;
 
     // 条件1: 子域名前缀是 ip.
     const isIPSubdomain = host.startsWith("ip.");
 
     // 条件2: 路径是 /ip
-    const isIPPath = path === "/ip" || path.startsWith("/ip/");
+    const isIPPath = path === "/ip" || path.startsWith("/ip/") || params.has("ip");
 
     if (isIPSubdomain || isIPPath) {
       return getIPInfo(request);
